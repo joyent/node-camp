56da1a4eceb6434e4ad4dcfa976d8e2baed1a0e9
Tim Caswell <tim.caswell@palm.com>
2010-11-24 00:03:59 -0800

    Add an icon for phone menus.

--- examples/exploder/server.js
+++ examples/exploder/server.js
@@ -1,9 +1,11 @@
 
 var Connect = require('connect');
 
+var web = __dirname + "/web";
+
 Connect.createServer(
   Connect.logger(),
-  Connect.staticProvider(__dirname + "/web")
+  Connect.staticProvider(web)
 ).listen(8080);
 
 console.log("Exploder server running at http://localhost:8080");
