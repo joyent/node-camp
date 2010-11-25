84f984a5fa5c53f70ebaa5215d57e9f33bf11e4e
Tim Caswell <tim.caswell@palm.com>
2010-11-24 17:12:04 -0800

    Make exploder actually a game with a goal
    Change-Id: I5d52f0d90f794c228a6b268ef13fc7fa3976daf8

--- examples/exploder/web/game.js
+++ examples/exploder/web/game.js
@@ -4,7 +4,10 @@ Engine('sprites', function (engine) {
   var Sprite = engine.Sprite;
   var colors = ['green', 'blue', 'brown', 'white', 'yellow', 'orange',
                 'purple', 'red', 'grey'];
+  // Grid used for quick collision detection
+  var ballGrid = {};
 
+  // Watch the height of the window.
   var width, height;
   function onResize() {
     width = document.width - 48;
@@ -23,10 +26,36 @@ Engine('sprites', function (engine) {
   }
   Spark.prototype.animate = function (delta) {
     this.life -= delta;
+    this.x += this.mx * delta / 1000;
+    this.y += this.my * delta / 1000;
+    this.checkCollisions(ballGrid, 40);
     if (this.life < 0 ||
         this.x < -48 || this.y < -48 ||
         this.x > width + 48 || this.y > height + 48) {
       this.hide();
+    }
+  };
+  Spark.prototype.checkCollisions = function (grid, distance) {
+    var cx = Math.floor(this.x / 48);
+    var cy = Math.floor(this.y / 48);
+    var self = this;
+    function checkBall(ball) {
+      var d = Math.sqrt((ball.x - self.x) * (ball.x - self.x) +
+                        (ball.y - self.y) * (ball.y - self.y));
+      if (d <= distance) {
+        ball.mx += self.mx / 10;
+        ball.my += self.my / 10;
+        self.life = -1;
+      }
+
+    }
+    for (var gx = cx - 1; gx <= cx + 1; gx++) {
+      for (var gy = cy - 1; gy <= cy + 2; gy++) {
+        var key = gx + "," + gy;
+        if (grid.hasOwnProperty(key)) {
+          grid[key].forEach(checkBall);
+        }
+      }
     }
   };
   Sprite.adopt(Spark);
@@ -34,36 +63,82 @@ Engine('sprites', function (engine) {
   function Ball(x, y, angle) {
     var n = Math.floor(angle / 360 * colors.length);
     Sprite.call(this, x, y, 0, colors[n]);
-    angle *= angleToRadian;
-    this.mx = Math.sin(angle) * 200;
-    this.my = -Math.cos(angle) * 200;
+    this.mx = 0;
+    this.my = 0;
+    this.show();
+    this.updateGrid();
   }
+  Ball.prototype.hide = function () {
+    Sprite.prototype.hide.call(this);
+    this.updateGrid();
+  };
+  Ball.prototype.updateGrid = function () {
+    // Remove the old entry in the collision grid
+    if (this.gx !== undefined) {
+      var list = ballGrid[this.gx + "," + this.gy];
+      list.splice(list.indexOf(this), 1);
+    }
+    if (!this.div) {
+      return;
+    }
+    var gx = this.gx = Math.floor(this.x / 48);
+    var gy = this.gy = Math.floor(this.y / 48);
+    var key = gx + "," + gy;
+    if (!ballGrid.hasOwnProperty(key)) {
+      ballGrid[key] = [];
+    }
+    ballGrid[key].push(this);
+  };
   Ball.prototype.animate = function (delta) {
+    this.x += this.mx * delta / 1000;
+    this.y += this.my * delta / 1000;
+    if (this.mx || this.my) {
+      this.updateGrid();
+    }
     if (this.x < 0) {
       this.x *= -1;
       this.mx *= -1;
+      this.checkBounce();
     } else if (this.x > width) {
       this.x = 2 * width - this.x;
       this.mx *= -1;
+      this.checkBounce();
     }
     if (this.y < 0) {
       this.y *= -1;
       this.my *= -1;
+      this.checkBounce();
     } else if (this.y > height) {
       this.y = 2 * height - this.y;
       this.my *= -1;
+      this.checkBounce();
     }
+  };
+  Ball.prototype.checkBounce = function () {
+    var speed = Math.sqrt(this.mx * this.mx + this.my * this.my);
+    if (speed > 400) {
+      this.hide();
+      for (var i = 0; i < 30; i++) {
+        explode({
+          clientX: this.x + 24,
+          clientY: this.y + 24
+        });
+      }
+    }
+
   };
   Sprite.adopt(Ball);
 
   // Create a few balls on the screen
   for (var i = 1; i < 360; i += 12) {
-    (new Ball(
-      Math.floor(width / 2),
-      Math.floor(height / 2),
+    var rad = i * angleToRadian;
+    var ball = new Ball(
+      width / 2 + Math.sin(rad) * 100,
+      height / 2 + Math.cos(rad) * 100,
       i
-    )).show();
+    );
   }
+
   window.addEventListener('resize', onResize, true);
 
   var down = false;
@@ -97,6 +172,12 @@ Engine('sprites', function (engine) {
   }
   var fpsDiv = document.getElementById('fps');
   engine.on('animate', function (delta) {
+    if (!engine.sprites.length) {
+      explode({
+        clientX: width / 2,
+        clientY: height / 2
+      });
+    }
     fpsDiv.innerText = (Math.floor(1000 / delta) / 1) + " fps";
     if (fire) {
       explode(fire);
