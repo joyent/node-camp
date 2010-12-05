e428cecd703cefb1dc6d5319376af940603bcfb8
Tim Caswell <tim.caswell@palm.com>
2010-12-04 17:17:34 -0800

    Clean up the code a bit got the game
    Change-Id: Iac2b9c7d7dc197fb7ca525a5f09b37e784a4a138

--- examples/hexes/web/client.js
+++ examples/hexes/web/client.js
@@ -1,4 +1,4 @@
-/*global Sprite io*/
+/*global Sprite io document*/
 
 var colors = ['space', 'red', 'brown', 'purple', 'blue', 'orange', 
               'green', 'yellow'];
@@ -20,10 +20,93 @@ socket.on('disconnect', function () {
   flail = true;
 });
 
+function inherits(child, parent) {
+  child.prototype = Object.create(parent.prototype);
+  child.prototype.constructor = child;
+  child.parent = parent;
+}
 
-
+// Singleton selection sprite
+var selected;
 
 var pieces = {};
+
+
+
+function Tile(x, y, colorCode) {
+  Sprite.call(this, x, y, colors[colorCode]);
+}
+inherits(Tile, Sprite);
+Tile.prototype.setTransform = function (x, y) {
+  x = x * 55 + 10;
+  y = 47 + y * 64 - (x % 2) * 32;
+  Sprite.prototype.setTransform.call(this, x, y);
+};
+
+function Space(x, y) {
+  this.gx = x;
+  this.gy = y;
+  Tile.call(this, x, y, 0);
+}
+inherits(Space, Tile);
+
+Space.prototype.onClick = function (evt) {
+  if (selected) {
+    socket.send({move: {id: selected.id, x: this.gx, y: this.gy}});
+    selected.deselect();
+  }
+};
+
+function Piece(x, y, colorCode) {
+  if (pieces.hasOwnProperty(colorCode)) {
+    pieces[colorCode].moveTo(x, y);
+    return pieces[colorCode];
+  }
+  Tile.call(this, x, y, colorCode);
+  this.id = colorCode;
+  pieces[colorCode] = this;
+}
+inherits(Piece, Tile);
+
+Piece.prototype.renderDiv = function () {
+  var div = Tile.prototype.renderDiv.apply(this, arguments);
+  var child = document.createElement('div');
+  this.child = child;
+  child.style.display = "none";
+  div.appendChild(child);
+  return div;
+};
+
+Piece.prototype.select = function () {
+  if (selected) {
+    selected.deselect();
+  }
+  selected = this;
+  this.child.style.display = "block";
+};
+
+Piece.prototype.deselect = function () {
+  if (selected === this) {
+    selected = null;
+  }
+  this.child.style.display = "none";
+};
+
+Piece.prototype.onClick = Piece.prototype.select;
+
+Piece.prototype.destroy = function () {
+  if (selected === this) {
+    selected = null;
+  }
+  delete pieces[this.id];
+  Tile.prototype.destroy.call(this);
+};
+var zIndex = 1000;
+Piece.prototype.moveTo = function () {
+  this.div.style.zIndex = zIndex ++;
+  Sprite.prototype.moveTo.apply(this, arguments);
+};
+
 
 var Commands = {
   map: function (map) {
@@ -33,8 +116,8 @@ var Commands = {
     });
   },
   move: function (params) {
-    if (selection && params.id === selection.piece.id) {
-      selection.destroy();
+    if (selected && params.id === selected.id) {
+      selected.deselect();
     }
     pieces[params.id].moveTo(params.x, params.y);
   }
@@ -51,77 +134,6 @@ socket.on('message', function (message) {
     }
   });
 });
-
-// Singleton selection sprite
-var selection;
-function Selection(piece) {
-  if (selection) {
-    selection.destroy();
-  }
-  selection = this;
-  Sprite.call(this, piece.x, piece.y, 'highlight');
-  this.div.style.zIndex = zIndex ++;
-  this.piece = piece;
-}
-// Hook into destroy to remove reference from selection
-Selection.prototype.destroy = function () {
-  Sprite.prototype.destroy.call(this);
-  selection = null;
-};
-// Destroy on click
-Selection.prototype.onClick = Selection.prototype.destroy;
-
-function Tile(x, y, colorCode) {
-  Sprite.call(this, x, y, colors[colorCode]);
-}
-Tile.prototype.setTransform = function (x, y) {
-  x = x * 55 + 10;
-  y = 47 + y * 64 - (x % 2) * 32;
-  Sprite.prototype.setTransform.call(this, x, y);
-};
-
-function Space(x, y) {
-  this.gx = x;
-  this.gy = y;
-  Tile.call(this, x, y, 0);
-}
-
-Space.prototype.onClick = function (evt) {
-  if (selection) {
-    socket.send({move: {id: selection.piece.id, x: this.gx, y: this.gy}});
-    selection.destroy();
-  }
-};
-
-function Piece(x, y, colorCode) {
-  if (pieces.hasOwnProperty(colorCode)) {
-    pieces[colorCode].moveTo(x, y);
-    return pieces[colorCode];
-  }
-  Tile.call(this, x, y, colorCode);
-  this.id = colorCode;
-  pieces[colorCode] = this;
-}
-
-Piece.prototype.onClick = function (evt) {
-  new Selection(this);
-};
-Piece.prototype.destroy = function () {
-  delete pieces[this.id];
-  Tile.prototype.destroy.call(this);
-};
-var zIndex = 1000;
-Piece.prototype.moveTo = function () {
-  this.div.style.zIndex = zIndex ++;
-  Sprite.prototype.moveTo.apply(this, arguments);
-};
-
-// Set up inheritance
-Space.prototype.__proto__ = Tile.prototype;
-Piece.prototype.__proto__ = Tile.prototype;
-Tile.prototype.__proto__ = Sprite.prototype;
-Selection.prototype.__proto__ = Sprite.prototype;
-
 for (var x = 0; x < 5; x++) {
   for (var y = 0; y < (6 + x % 2); y++) {
     new Space(x, y);
