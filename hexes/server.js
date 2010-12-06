2fde0a1aa27a2cf1717a765d1542d15ada2cdbd4
Tim Caswell <tim.caswell@palm.com>
2010-12-06 12:50:13 -0800

    Merge the files into a single server.js and client.js
    Change-Id: Ida0a6b8b01e8a8fe0934d7e3d65cebe75e557b34

--- hexes/server.js
+++ hexes/server.js
@@ -1,17 +1,55 @@
 var Connect = require('connect'),
-    io = require('socket.io'),
-    game = require('./web/game');
+    io = require('socket.io');
 
+// Allow the user to specify the port via environment variables
 var PORT = process.env.PORT || 8080;
 
+// Serve static files in the web folder
 var server = Connect.createServer(
-  Connect.staticProvider(__dirname + "/web")
+  Connect.staticProvider('web')
 );
 
+// Listen to socket.io traffic too
 var socket = io.listen(server);
 
-game.socket = socket;
-socket.on('connection', game);
+// Keep a mapping of all the pieces in the game
+var map = {
+  1: {x: 1, y: 0},
+  2: {x: 3, y: 1},
+  3: {x: 1, y: 2},
+  4: {x: 3, y: 3},
+  5: {x: 1, y: 4},
+  6: {x: 3, y: 5},
+  7: {x: 1, y: 6}
+};
+
+// Every time a new client connects or reconnects, we get this
+socket.on('connection', function (client) {
+  
+  // Send the client te initial map
+  client.send({map: map});
+
+  // Define the commands we're willing to accept from the client
+  var Commands = {
+    // In this simple example, we re-broadcast the move to all clients.
+    // In a real game there would some rule checking and other logic here.
+    move: function (params) {
+      map[params.id] = params;
+      socket.broadcast({move: params});
+    }
+  };
+
+  // Route messages to the Commands object
+  client.on('message', function (message) {
+    Object.keys(message).forEach(function (command) {
+      if (Commands.hasOwnProperty(command)) {
+        Commands[command](message[command]);
+      } else {
+        console.error("Invalid command " + command);
+      }
+    });
+  });
+});
 
 server.listen(PORT);
 console.log("Server running at port http://localhost:%s/", PORT);
--- hexes/web/client.js
+++ hexes/web/client.js
@@ -1,3 +1,235 @@
+/*global document window PalmSystem io Commands*/
+
+function Sprite(x, y, className) {
+  var div = this.renderDiv(className);
+  div.sprite = this;
+  this.setTransform(x, y);
+  if (Sprite.container) {
+    Sprite.container.appendChild(div);
+  } else {
+    Sprite.pending.push(div);
+  }
+}
+Sprite.pending = [];
+
+Sprite.prototype.renderDiv = function (className) {
+  var div = this.div = document.createElement('div');
+  div.setAttribute('class', className);
+  return div;
+};
+
+// Move the sprite to a specified offset using hardware accel.
+Sprite.prototype.setTransform = function (x, y) {
+  this.x = x;
+  this.y = y;
+  this.div.style.webkitTransform = "translate3d(" + x + "px, " + y + "px, 0)";
+};
+
+// Move the sprite, but animate over a time lapse
+Sprite.prototype.moveTo = function (x, y, time) {
+  time = time || 1;
+  this.div.style.webkitTransitionDuration = time + "s";
+  this.setTransform(x, y);
+};
+
+Sprite.prototype.destroy = function () {
+  if (Sprite.container) {
+    Sprite.container.removeChild(this.div);
+  } else {
+    Sprite.pending.splice(Sprite.pending.indexOf(this.div), 1);
+  }
+};
+
+window.addEventListener('load', function () {
+
+  // Find the container and append any pending divs
+  var container = Sprite.container = document.getElementById('sprites');
+  Sprite.pending.forEach(function (div) {
+    container.appendChild(div);
+  });
+  var width = container.clientWidth,
+      height = container.clientHeight;
+
+  function onResize() {
+    var winWidth = window.innerWidth,
+        winHeight = window.innerHeight;
+    var vertical = (height > width) === (winHeight > winWidth);
+    var transform;
+    if (vertical) {
+      transform = "scale(" + 
+        Math.min(winWidth / width, winHeight / height) + ")";
+    } else {
+      transform = "scale(" + 
+        Math.min(winWidth / height, winHeight / width) + ") rotate(-90deg)";
+    }
+    container.style.webkitTransform = transform;
+  }
+  window.addEventListener('resize', onResize);
+  onResize();
+
+  delete Sprite.pending;
+
+  function findSprite(target) {
+    if (target === container) {
+      return;
+    }
+    if (target.sprite) {
+      return target.sprite;
+    }
+    if (!target.parentNode) {
+      return;
+    }
+    return findSprite(target.parentNode);
+  }
+
+  var start;
+  var a;
+  // Listen for mouse and touch events
+  function onDown(evt) {
+    start = findSprite(evt.target);
+
+    if (!start) {
+      return;
+    }
+    evt.stopPropagation();
+    evt.preventDefault();
+    if (start.onDown) {
+      start.onDown(evt);
+    }
+  }
+  function onMove(evt) {
+    if (!start) {
+      return;
+    }
+    evt.stopPropagation();
+    evt.preventDefault();
+    if (start.onMove) {
+      start.onMove(evt);
+    }
+  }
+  function onUp(evt) {
+    if (!start) {
+      return;
+    }
+    evt.stopPropagation();
+    evt.preventDefault();
+    if (start.onUp) {
+      start.onUp(evt);
+    } else if (start.onClick) {
+      var end = findSprite(evt.target);
+      if (start === end) {
+        start.onClick(evt);
+      }
+    }
+    start = undefined;
+  }
+  container.addEventListener('mousedown', onDown, false);
+  document.addEventListener('mousemove', onMove, false);
+  document.addEventListener('mouseup', onUp, false);
+  container.addEventListener('touchstart', function (e) {
+    if (e.touches.length === 1) {
+      var touch = e.touches[0];
+      touch.stopPropagation = function () {
+        e.stopPropagation();
+      };
+      touch.preventDefault = function () {
+        e.preventDefault();
+      };
+      onDown(touch);
+    }
+  }, false);
+  document.addEventListener('touchmove', function (e) {
+    if (e.touches.length === 1) {
+      var touch = e.touches[0];
+      touch.stopPropagation = function () {
+        e.stopPropagation();
+      };
+      touch.preventDefault = function () {
+        e.preventDefault();
+      };
+      onMove(touch);
+    }
+  }, false);
+  document.addEventListener('touchend', onUp, false);
+
+});
+
+
+// Start the palm system if we're in a webOS app
+if (typeof PalmSystem !== 'undefined') {
+  window.addEventListener('load', function () {
+    PalmSystem.stageReady();
+    if (PalmSystem.enableFullScreenMode) {
+      PalmSystem.enableFullScreenMode(true);
+    }
+  }, true);
+}
+
+// Define Object.create if the browser doesn't have it already
+if (!Object.hasOwnProperty('create')) {
+  (function () {
+    function F() {}
+    Object.create = function (parent) {
+      F.prototype = parent;
+      return new F();
+    };
+  }());
+}
+
+
+
+// Set up a socket.io channel with the backend server
+var socket;
+if (typeof PalmSystem !== 'undefined') {
+  // webOS apps are special since the static files don't come from the server
+  socket = new io.Socket("creationix.com", {
+    port: 8080,
+    transports: ['xhr-polling']
+  });
+} else {
+  socket = new io.Socket();
+}
+
+window.addEventListener('load', function () {
+
+  var flail = true;
+  function tryConnect() {
+    if (flail) {
+      socket.connect();
+    }
+  }
+  setTimeout(tryConnect);
+  setInterval(tryConnect, 10000);
+  socket.on('connect', function () {
+    flail = false;
+  });
+  socket.on('disconnect', function () {
+    console.error("Got disconnected from server!");
+    flail = true;
+  });
+});
+
+// Super small OOP library for easy inheritance
+function inherits(child, parent) {
+  child.prototype = Object.create(parent.prototype);
+  child.prototype.constructor = child;
+  child.parent = parent;
+}
+
+// Forward messages from the backend to the Commands object in the client
+socket.on('message', function (message) {
+  //console.log(message);
+  if (!Commands) {
+    return;
+  }
+  Object.keys(message).forEach(function (command) {
+    if (Commands.hasOwnProperty(command)) {
+      Commands[command](message[command]);
+    } else {
+      console.error("Invalid command " + command);
+    }
+  });
+});
 /*global document Sprite socket inherits*/
 
 var colors = ['space', 'red', 'brown', 'purple', 'blue', 'orange', 
